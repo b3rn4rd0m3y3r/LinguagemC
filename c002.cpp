7bc1910227281d56180f69af3bce2e08a10bfc3f
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-01-10 17:52:52 -0300

    Add files via upload

---
+++ c002.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char *argv[]){
+	char nome[50];
+	cout << "Digite o seu nome:";
+	cin >> nome;
+	cout << "Seu nome é ";
+	cout << nome;
+	system("PAUSE");
+	return EXIT_SUCCESS;
+	}
